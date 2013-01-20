4113dc01542520a2ff3b546784b298b6326a0459
Gregor Best <gbe@ring0.de>
2013-01-20 13:20:52 +0100

    small header fix
    Signed-off-by: Gregor Best <gbe@ring0.de>

--- menu.py
+++ menu.py
@@ -30,7 +30,7 @@ def header(login, active="home"):
 			s += "active" if active == "logout" else ""
 			s += "\"><a href=\"/?action=logout\">Logout</a></li>"
 		else:
-			s += "<li class=\"active\">Login</li>"
+			s += "<li class=\"active\"><a href=\"#\">Login</a></li>"
 		s += "<li class=\"\"><a href=\"/admin\">Admin</a></li>"
 	else:
 		s += "<li class=\"\"><a href=\"/\">User-Mode</a></li>"
