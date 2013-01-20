57ccb7cf10877a991e4683fa7d103e5639007956
Gregor Best <gbe@ring0.de>
2013-01-20 13:47:52 +0100

    Translate interface to german
    Signed-off-by: Gregor Best <gbe@ring0.de>

--- menu.py
+++ menu.py
@@ -68,10 +68,9 @@ def admin(conf, conn):
 		conn.commit()
 
 	cursor.execute("SELECT count(*), sum(paid), sum(there) FROM users")
-	result = cursor.fetchone()
-	print("<h1>%d Users (%d paid, %d there)</h1>" % result)
+	print("<h1>%d Benutzer (%d haben bezahlt, %d sind da)</h1>" % cursor.fetchone())
 	print("<div><table>")
-	print(html.tb_row(["Name", "eMail", "Delete", "Paid", "Is there", "Shirts"], head=True))
+	print(html.tb_row(["Name", "eMail", "L&ouml;schen", "Hat bezahlt", "Ist da", "Shirts"], head=True))
 	cursor.execute("SELECT u_id, name, email, paid, there FROM users");
 	for x in cursor.fetchall():
 		user = {
@@ -87,26 +86,26 @@ def admin(conf, conn):
 		tbl_del = "<a href=\"?action=delete&user=" + urllib.quote(user["name"]) + "\">Delete</a>"
 		tbl_paid = "<a href=\"?action=setpaid&user=" + urllib.quote(user["name"]) + "&value="
 		if user["has_paid"]:
-			tbl_paid += "no\">yes</a>"
+			tbl_paid += "no\">ja</a>"
 		else:
-			tbl_paid += "yes\">no</a>"
+			tbl_paid += "yes\">nein</a>"
 		tbl_there = "<a href=\"?action=setthere&user=" + urllib.quote(user["name"]) + "&value="
 		if user["is_there"]:
-			tbl_there += "no\">yes</a>"
+			tbl_there += "no\">ja</a>"
 		else:
-			tbl_there += "yes\">no</a>"
+			tbl_there += "yes\">nein</a>"
 		print(html.tb_row([user["name"], user["email"], tbl_del, tbl_paid, tbl_there, user["shirts"]]))
 	print("</table></div>")
 
-	print("<h1>Lunch orders</h1>")
+	print("<h1>Frystyck</h1>")
 	print("<div><table>")
-	print(html.tb_row(["Buns", "Baloney", "Cheese", "Jam", "Cornflakes"], head=True))
+	print(html.tb_row(["Br&ouml;tchen", "Wurscht", "K&auml;se", "Marmelade", "Cornflakes"], head=True))
 	cursor.execute("SELECT sum(buns), sum(baloney), sum(cheese), sum(jam), sum(cornflakes) FROM lunch")
 	print(html.tb_row(cursor.fetchone()))
 	print("</table></div>")
 
 	print("<h1>Admins</h1>")
-	print("<h2>Registered admins</h2>")
+	print("<h2>Registrierte Admins</h2>")
 	print("<div><ul>")
 	try:
 		fh = open("../passwd", "r")
@@ -117,7 +116,8 @@ def admin(conf, conn):
 		pass
 	print("</ul></div>")
 
-	print("<div><h2>Add a new admin</h2>")
+	print("<div><h2>Neuen Admin hinzuf&uuml;gen</h2>")
+	print("<p>Ich bin zu faul das zu &uuml;bersetzen... (gbe)</p>")
 	print("<p>Admins are registerd in <code>/www/register/passwd</code>, which is a htpasswd file. To add a new admin do the following:")
 	print("<pre>")
 	print("$ ssh root@eh13.c3pb.de")
@@ -145,27 +145,27 @@ def main(login, conf, conn):
 		c = conn.cursor()
 		c.execute("SELECT count(*) FROM users")
 		if c.fetchone()[0] < conf.get("maxusers"):
-			print("<div><h2>New user</h2>")
+			print("<div><h2>Neuer Benutzer</h2>")
 			print("<form method=\"POST\" class=\"form-horizontal\">")
 			print(html.f_hidden("action", "add_user"))
 			print(html.form_input("new_name", "Name", "username"))
 			print(html.form_input("new_mail", "E-Mail", "email"))
-			print(html.form_password("new_pass", "Password", "password"))
-			print(html.form_password("new_pass2", "Password (again)", "password_again"))
+			print(html.form_password("new_pass", "Passwort", "password"))
+			print(html.form_password("new_pass2", "Passwort (nochmal)", "password_again"))
 			print(html.form_submit())
 			print("</form></div>")
 		else:
-			print("<div><h2>Registration closed</h2>")
-			print("Registration is closed, there are already " + str(conf.get("maxusers")) + " people registered. Sorry :(")
+			print("<div><h2>Registrierung geschlossen</h2>")
+			print("Die Registrierung ist geschlossen. Es sind schon " + str(conf.get("maxusers")) + " Wesen registriert. Sorry :(")
 			print("</div>")
 		return
 
 	user = login.as_dict()
 
-	print("<div><h2>User status for " + user["name"] + "</h2>")
+	print("<div><h2>Status f&uuml;r " + user["name"] + "</h2>")
 	print("<table>")
-	print(html.tb_row(["Payment received", ("Yes" if user["has_paid"] else "No")]))
-	print(html.tb_row(["Is there", ("Yes" if user["is_there"] else "No")]))
+	print(html.tb_row(["Bezahlt", ("Yes" if user["has_paid"] else "No")]))
+	print(html.tb_row(["Anwesend", ("Yes" if user["is_there"] else "No")]))
 	print(html.tb_row(["Email", user["email"]]))
 	print("</table>")
 	print("</div>")
@@ -180,7 +180,7 @@ def main(login, conf, conn):
 		items.append("<a href=\"?order=add_" + size + "\">+</a>")
 		return html.tb_row(items)
 
-	print("<div><h2>Shirt order</h2>")
+	print("<div><h2>Shirts</h2>")
 	print("<table>")
 	print(shirt_order_row("S"))
 	print(shirt_order_row("M"))
@@ -190,20 +190,20 @@ def main(login, conf, conn):
 	print("</table>")
 	print("<form method=\"POST\">")
 	print(html.f_hidden("order", "clear_all"))
-	print(html.f_submit("Clear order"))
+	print(html.f_submit("Zur&uuml;cksetzen"))
 	print("</form>")
 	print("</div>")
 
-	print("<div><h2>Lunch order (per day)</h2>")
+	print("<div><h2>Frystyck (pro Tag)</h2>")
 	print("<form method=\"POST\">")
 	print(html.f_hidden("action", "update_lunch"))
 	print("<table>")
-	print(html.tb_row(["Buns", html.f_input("replaceme", "buns", value=user["lunch"]["buns"], size=10)]))
-	print(html.tb_row(["Baloney", html.f_checkbox("food", "baloney", user["lunch"]["baloney"])]))
-	print(html.tb_row(["Cheese", html.f_checkbox("food", "cheese", user["lunch"]["cheese"])]))
-	print(html.tb_row(["Jam", html.f_checkbox("food", "jam", user["lunch"]["jam"])]))
+	print(html.tb_row(["Br&ouml;tchen", html.f_input("replaceme", "buns", value=user["lunch"]["buns"], size=10)]))
+	print(html.tb_row(["Wurst", html.f_checkbox("food", "baloney", user["lunch"]["baloney"])]))
+	print(html.tb_row(["K&auml;se", html.f_checkbox("food", "cheese", user["lunch"]["cheese"])]))
+	print(html.tb_row(["Marmelade", html.f_checkbox("food", "jam", user["lunch"]["jam"])]))
 	print(html.tb_row(["Cornflakes", html.f_checkbox("food", "cornflakes", user["lunch"]["cornflakes"])]))
 	print("</table>")
-	print(html.f_submit("Update"))
+	print(html.f_submit("Aktualisieren"))
 	print("</form>")
 	print("</div>")
