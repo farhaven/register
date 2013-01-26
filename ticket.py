a21d8cfc470bccbf51a0b8eb648d39895f5a296d
Helge Jung <helge.jung@youngage.eu>
2013-01-26 15:34:05 +0100

    new ticket template

--- ticket.py
+++ ticket.py
@@ -1,4 +1,6 @@
 import qrencode
+import os
+import shutil
 from pyPdf import PdfFileReader, PdfFileWriter
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
@@ -36,10 +38,15 @@ def build_ticket(ticket, owner = None, \
 	output.addPage(page)
 	output.write(file(outputFile[1],"w"))
 
-	#File.delete(tempName)
+	os.remove(tempName)
 	return outputFile
 
 if __name__ == "__main__":
 	# TODO read command line (oder so)
-	print build_ticket("12345-67890-12345-67890-12345-67890", "Bernd", ticket_x = 4, ticket_y = 10.9, ticket_size = 5.9, owner_x = 11.6, owner_y = 7.7)
+	ticket_no = "12345-67890-12345-67890-12345-67890"
+	ticket_owner = "Bernd"
+	output_name = "output.pdf"
+
+	result = build_ticket(ticket_no, ticket_owner, ticket_x = 2.58, ticket_y = 5.93, ticket_size = 3.14, owner_x = 7.2, owner_y = 7)
+	shutil.move(result[1], output_name)
 
