72e1569cafc1337d4500a8234c749db60ed6331b
Helge Jung <helge.jung@youngage.eu>
2013-01-26 16:23:39 +0100

    added ticket-id as string

--- ticket.py
+++ ticket.py
@@ -11,6 +11,7 @@ from StringIO import StringIO
 def build_ticket(ticket, owner = None, \
                  background = "ticket-template.pdf", \
                  ticket_x = 2, ticket_y = 2, ticket_size = 5, \
+	         ticket_print_as_string = True, ticket_sx = 2, ticket_sy = 7.5,
                  owner_x = 10, owner_y = 3):
 
 	tempImg = tempfile.mkstemp()
@@ -25,6 +26,10 @@ def build_ticket(ticket, owner = None, \
 	if owner != None:
 		overlay.setFont('Helvetica', 20)
 		overlay.drawString(owner_x * cm, owner_y * cm, owner)
+
+	if ticket_print_as_string:
+		overlay.setFont('Helvetica', 8)
+		overlay.drawString(ticket_sx * cm, ticket_sy * cm, ticket)
 
 	overlay.drawImage(tempName, ticket_x * cm, ticket_y * cm, ticket_size * cm, ticket_size * cm)
 	overlay.save()
@@ -47,6 +52,6 @@ if __name__ == "__main__":
 	ticket_owner = "Bernd"
 	output_name = "output.pdf"
 
-	result = build_ticket(ticket_no, ticket_owner, ticket_x = 2.58, ticket_y = 5.93, ticket_size = 3.14, owner_x = 7.2, owner_y = 7)
+	result = build_ticket(ticket_no, ticket_owner, ticket_x = 2.58, ticket_y = 5.93, ticket_size = 3.14, owner_x = 7.2, owner_y = 7, ticket_sx = 7.2, ticket_sy = 7.5)
 	shutil.move(result[1], output_name)
 
